e166dd1fcb500007fe64f46d545df93986206f5f
louisjournet91-code <louisjournet91@gmail.com>
2025-11-21 16:12:55 +0000

    Mise a jour finale

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ INITIAL_PORTFOLIO = {
     "Ticker": ["ESE.PA", "DCAM.PA", "PUST.PA", "CL2.PA", "BTC-EUR", "CASH"],
     "Nom": ["BNP S&P 500", "Amundi World", "Lyxor Nasdaq", "Amundi USA x2", "Bitcoin", "Liquidités"],
     "Type": ["ETF Action", "ETF Action", "ETF Tech", "ETF Levier", "Crypto", "Cash"],
-    "Quantité": [141.0, 716.0, 55.0, 176.0, 0.01275433, 510.84],
+    "Quantité": [141.0, 716.0, 55.0, 176.0, 0.0142413, 510.84],
     "PRU": [24.41, 4.68, 71.73, 19.71, 90165.46, 1.00]
 }
 
